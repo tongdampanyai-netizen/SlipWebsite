bad67c01f6d2f34a4ec213d3b6d89e50874d32fd
tongdampanyai-netizen <tongdampanyai@gmail.com>
2025-11-14 14:17:51 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,13 @@
+// firebase-config.js
 const firebaseConfig = {
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
+  apiKey: "AIzaSyA0QTKHs-r-2vvjUzvfPihRjwCZQ5nCmMw",
+  authDomain: "slipwebsite.firebaseapp.com",
+  projectId: "slipwebsite",
+  storageBucket: "slipwebsite.firebasestorage.app",
+  messagingSenderId: "263855349172",
+  appId: "1:263855349172:web:3024d9f6c9cb9409d1e6e1",
+  measurementId: "G-RBK9N2RQ48"
 };
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
